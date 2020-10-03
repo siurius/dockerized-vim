973e1feb519710fb58c8bff8a895f8e7ae3b156b
Yiran Li <yiranli91@gmail.com>
2020-10-03 11:05:07 +0800

    update home folder setting

--- run.sh
+++ run.sh
@@ -1,13 +1,14 @@
 #!/bin/bash
 
 DVIM_FOLDER=.dockerized-vim
+DVIM_HOME="/home/$USER"
 [ ! -d "${HOME}/${DVIM_FOLDER}/" ] && mkdir -p ${HOME}/${DVIM_FOLDER}
 
 # run docker
-docker run --rm -it --hostname nvim -u $(id -u) -e "HOME=$HOME" \
+docker run --rm -it --hostname nvim -u $(id -u) -e "HOME=$DVIN_HOME" \
     -v "/:/host" \
     -v "/etc/passwd:/etc/passwd:ro" -v "/etc/group:/etc/group:ro" \
-    -v "${HOME}/${DVIM_FOLDER}:${HOME}" \
+    -v "${HOME}/${DVIM_FOLDER}:${DVIM_HOME}" \
     -w "/host/$PWD" \
     yiranli/dockerized-vim \
     "$@"
