1628e4941f5379efe22b8add4d51c811ae93b312
Yiran Li <yiranli91@gmail.com>
2020-10-04 12:36:35 +0800

    debug for empty file

--- run.sh
+++ run.sh
@@ -3,7 +3,10 @@
 DVIM_FOLDER=.dockerized-vim
 DVIM_HOME="/home/$USER"
 [ ! -d "${HOME}/${DVIM_FOLDER}/" ] && mkdir -p ${HOME}/${DVIM_FOLDER}
-DVIM_FILE_PATH=/host$(readlink -f $1)
+DVIM_FILE_PATH=""
+if [ "$#" -ge 1 ]; then
+    DVIM_FILE_PATH=/host$(readlink -f $1)
+fi
 
 # run docker
 docker run --rm -it --hostname nvim -u $(id -u) -e "HOME=$DVIM_HOME" \
